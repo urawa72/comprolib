fa720a2f0cd8f6b185351ff8899ea75b3da09957
urawa72 <urawa72@gmail.com>
2020-08-30 09:19:52 +0900

    modify union_find, mod_comb

--- cpplib/mod_comb.cpp
+++ cpplib/mod_comb.cpp
@@ -1,7 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll MOD = 1e9 + 7;
+
+const int MAX = 1000000;
+const int MOD = 1000000007;
+
+// テーブルを作る前処理
+long long fac[MAX], finv[MAX], inv[MAX];
+void com_init() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+ // 二項係数計算
+long long com(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
 
 // 繰り返し二乗法
 ll mod_pow(ll x, ll a){
--- cpplib/union_find.cpp
+++ cpplib/union_find.cpp
@@ -1,35 +1,26 @@
-const int MAX_N = 100;
-int par[MAX_N];
-int rank[MAX_N];
+#include <bits/stdc++.h>
+using namespace std;
 
-// n要素で初期化
-void init(int n){
-    for(int i = 0; i < n; i++){
-        par[i] = i;
-        rank[i] = 0;
+struct UnionFind {
+    vector<int> d;
+    // n要素で初期化
+    UnionFind(int n): d(n,-1) {}
+    // 木の根を決める
+    int root(int x) {
+        if (d[x] < 0) return x;
+        return d[x] = root(d[x]);
     }
-}
-
-// 木の根を決める
-int find(int x){
-    if(par[x] == x) return x;
-    else return par[x] = find(par[x]);
-}
-
-// xとy属する集合を併合
-void unite(int x, int y){
-    x = find(x);
-    y = find(y);
-    if(x == y) return;
-    if(rank[x] < rank[y]){
-        par[x] = y;
-    }else{
-        par[y] = x;
-        if(rank[x] == rank[y]) rank[x]++;
+    // xとy属する集合を併合
+    bool unite(int x, int y) {
+        x = root(x); y = root(y);
+        if (x == y) return false;
+        if (d[x] > d[y]) swap(x,y);
+        d[x] += d[y];
+        d[y] = x;
+        return true;
     }
-}
-
-// xとyが同じ集合に属するか否か
-bool same(int x, int y){
-    return find(x) == find(y);
-}
+    // xとyが同じ集合に属するか否か
+    bool same(int x, int y) { return root(x) == root(y); }
+    // xの属する集合のサイズ
+    int size(int x) { return -d[root(x)];}
+};
